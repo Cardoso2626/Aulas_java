f5774a1bd3dbb58cfc74215767f75a9b8018b3c1
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-02-17 20:54:33 -0300

    Usando DTO e SERVICE

---
+++ src/main/java/br/com/fiap/api_rest/dto/LivroRequest.java
@@ -0,0 +1,28 @@
+package br.com.fiap.api_rest.dto;
+
+public class LivroRequest {
+    private String titulo;
+    private String autor;
+
+
+    public LivroRequest(String autor, String titulo) {
+        this.autor = autor;
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
