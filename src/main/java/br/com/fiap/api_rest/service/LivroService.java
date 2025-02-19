e08cfc20b732121148c813828d5b4fea8fa4ee1f
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-02-18 23:02:46 -0300

    Entendendo sobre validations e mudando requestes

--- src/main/java/br/com/fiap/api_rest/service/LivroService.java
+++ src/main/java/br/com/fiap/api_rest/service/LivroService.java
@@ -4,12 +4,22 @@ import br.com.fiap.api_rest.dto.LivroRequest;
 import br.com.fiap.api_rest.dto.LivroRequestDTO;
 import br.com.fiap.api_rest.dto.LivroResponse;
 import br.com.fiap.api_rest.model.Livro;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class LivroService {
     public Livro requestToLivro(LivroRequest livroRequest) {
         Livro livro = new Livro();
         livro.setAutor(livroRequest.getAutor());
         livro.setTitulo(livroRequest.getTitulo());
+        livro.setPreco(livroRequest.getPreco());
+        livro.setCategoria(livroRequest.getCategoria());
+        livro.setIsbn(livroRequest.getIsbn());
+
         return livro;
 
     }
@@ -24,5 +34,18 @@ public class LivroService {
 
         public LivroResponse livroToResponse(Livro livro){
             return new LivroResponse( livro.getAutor()    + "-" + livro.getTitulo());
+        }
+
+        public List<LivroResponse> livrosToResponse (List<Livro> livros){
+            List<LivroResponse> listaLivros = new ArrayList<>();
+            for (Livro livro : livros){
+                listaLivros.add(livroToResponse(livro));
+            }
+            return listaLivros;
+        }
+
+        public List<LivroResponse> livrosToResponse2 (List<Livro> livros){
+            return livros.stream().map(this::livroToResponse).collect(Collectors.toList());
+
         }
 }
